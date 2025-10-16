c779e508804a899fd32df8e811555843453ce9b0
rekha0suthar <rekha0suthar@gmail.com>
2025-10-16 10:52:15 +0530

    dynamic api base url added

--- frontend/src/services/adminAPI.js
+++ frontend/src/services/adminAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const adminAPI = axios.create({
-  baseURL: 'http://localhost:3000/api/admin',
+  baseURL: import.meta.env.VITE_API_URL + '/admin',
   headers: {
     'Content-Type': 'application/json',
   },
